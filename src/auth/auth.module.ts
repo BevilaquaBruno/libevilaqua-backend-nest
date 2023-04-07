a67280bf70df3883b81daea433a86bfc6d51b8fb
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-04-07 12:34:03 -0300

    removed constants and start using env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
+      secret: process.env['SECRET'],
       signOptions: { expiresIn: '86400s' },
     }),
   ],
