79515a5109c321077b98d2767087d4147afbee24
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-04-07 16:35:15 -0300

    add validations with class-validator

--- src/auth/dto/main-auth.dto.ts
+++ src/auth/dto/main-auth.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, Length, IsNotEmpty } from 'class-validator';
 export class MainAuthDto {
+  @IsEmail({}, { message: 'Informe um e-mail válido' })
+  @Length(7, 50, { message: 'Informe um e-mail válido' })
   email: string;
+
+  @IsNotEmpty({ message: 'Informe a senha' })
   password: string;
 }
