3e26be644d2d45a6f90a7f0bdff6050113bcc623
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-04-07 15:07:04 -0300

    add env at run_migration fie

--- database/run_migrations.ts
+++ database/run_migrations.ts
@@ -3,13 +3,17 @@ import { DataSource } from 'typeorm';
 import { UserCreateTable1680738977433 } from './migrations/1680738977433-UserCreateTable';
 import { GenreCreateTable1680885361688 } from './migrations/1680885361688-GenreCreateTable';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'a',
-  password: 'a',
-  database: 'a',
+  host: process.env['DB_HOST'],
+  port: Number(process.env['DB_PORT']),
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD'],
+  database: process.env['DB_DATABASE'],
   migrations: [UserCreateTable1680738977433, GenreCreateTable1680885361688],
   migrationsTableName: 'migrations',
 });
