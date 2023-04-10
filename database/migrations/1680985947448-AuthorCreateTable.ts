e57d7efb481de884339a8d2cc570edeb9672289f
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-04-09 21:25:06 -0300

    fix pk author case

--- database/migrations/1680985947448-AuthorCreateTable.ts
+++ database/migrations/1680985947448-AuthorCreateTable.ts
@@ -1,20 +1,18 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AuthorCreateTable1680985947448 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE author(
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE author(
             id INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(60) NOT NULL,
             birth_date DATE DEFAULT NULL,
             death_date DATE DEFAULT NULL,
             bio TEXT,
-            CONSTRAINT pk_AUTHOR PRIMARY KEY (id)
+            CONSTRAINT PK_author PRIMARY KEY (id)
         );`);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE author;`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE author;`);
+  }
 }
