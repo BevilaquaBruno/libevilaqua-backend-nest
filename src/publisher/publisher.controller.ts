9dec8fe6e1e68bfc7364ebc3fd02458c734643a4
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-04-11 19:56:26 -0300

    add UseGuard to controllers

--- src/publisher/publisher.controller.ts
+++ src/publisher/publisher.controller.ts
@@ -6,30 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PublisherService } from './publisher.service';
 import { CreatePublisherDto } from './dto/create-publisher.dto';
 import { UpdatePublisherDto } from './dto/update-publisher.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('publisher')
 export class PublisherController {
   constructor(private readonly publisherService: PublisherService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createPublisherDto: CreatePublisherDto) {
     return this.publisherService.create(createPublisherDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.publisherService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.publisherService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +44,7 @@ export class PublisherController {
     return this.publisherService.update(+id, updatePublisherDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.publisherService.remove(+id);
