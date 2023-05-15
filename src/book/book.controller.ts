3155e8f11b200dca3bd0af04c5d2cfae03754723
Bruno Fernando Bevilaqua <bbbevilaqua@gmail.com>
2023-05-15 20:21:16 -0300

    add get by tags, genres, publisher and types

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
   HttpException,
   HttpStatus,
+  Query,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { BookService } from './book.service';
@@ -18,6 +19,46 @@ import { UpdateBookDto } from './dto/update-book.dto';
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @UseGuards(AuthGuard)
+  @Get('tags')
+  tags(@Query('ids') tags: string) {
+    const tagList: number[] = tags.split(',').map((v) => {
+      return parseInt(v);
+    });
+
+    return this.bookService.findBooksFromTags(tagList);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('genres')
+  genres(@Query('ids') genres: string) {
+    const genreList: number[] = genres.split(',').map((v) => {
+      return parseInt(v);
+    });
+
+    return this.bookService.findBooksFromGenres(genreList);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('publishers')
+  publishers(@Query('ids') publishers: string) {
+    const publisherList: number[] = publishers.split(',').map((v) => {
+      return parseInt(v);
+    });
+
+    return this.bookService.findBooksFromPublishers(publisherList);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('types')
+  types(@Query('ids') types: string) {
+    const typeList: number[] = types.split(',').map((v) => {
+      return parseInt(v);
+    });
+
+    return this.bookService.findBooksFromTypes(typeList);
+  }
 
   @UseGuards(AuthGuard)
   @Post()
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Raw, Repository } from 'typeorm';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { Book } from './entities/book.entity';
@@ -41,5 +41,30 @@ export class BookService {
       .leftJoin('book.authors', 'authorsForFilter')
       .where('authorsForFilter.id IN (:...authors)', { authors: [authorId] })
       .getMany();
+  }
+
+  findBooksFromTags(tagList: number[]) {
+    return this.bookServiceRepository
+      .createQueryBuilder('book')
+      .leftJoinAndSelect('book.genre', 'genre')
+      .leftJoinAndSelect('book.publisher', 'publisher')
+      .leftJoinAndSelect('book.type', 'type')
+      .leftJoinAndSelect('book.tags', 'tags')
+      .leftJoinAndSelect('book.authors', 'authors')
+      .leftJoin('book.tags', 'tagsForFilter')
+      .where('tagsForFilter.id IN (:...tags)', { tags: tagList })
+      .getMany();
+  }
+
+  findBooksFromGenres(genreList: number[]) {
+    return this.bookServiceRepository.findBy({ genre: In(genreList) });
+  }
+
+  findBooksFromPublishers(publisherList: number[]) {
+    return this.bookServiceRepository.findBy({ publisher: In(publisherList) });
+  }
+
+  findBooksFromTypes(typeList: number[]) {
+    return this.bookServiceRepository.findBy({ type: In(typeList) });
   }
 }
