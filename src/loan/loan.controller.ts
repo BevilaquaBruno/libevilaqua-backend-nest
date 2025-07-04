e3b54f7df7922d5d180f418896091caeb3704dda
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-07-04 20:50:17 -0300

    refactor loan create endpoint

--- src/loan/loan.controller.ts
+++ src/loan/loan.controller.ts
@@ -20,7 +20,7 @@ import { BookService } from 'src/book/book.service';
 import { PersonService } from 'src/person/person.service';
 import { FindLoanDto } from './dto/find-loan.dto';
 import { FindLoanHistoryDto } from './dto/find-loan-history.dto';
-import { find } from 'rxjs';
+import moment from 'moment';
 
 @Controller('loan')
 export class LoanController {
@@ -33,18 +33,78 @@ export class LoanController {
   @UseGuards(AuthGuard)
   @Post()
   async create(@Body() createLoanDto: CreateLoanDto) {
-    
     const isBookLoaned = await this.loanService.findLoanedBook(
       createLoanDto.bookId,
     );
-    
     if (isBookLoaned[1] != 0) {
       throw new HttpException(
         'Este livro já está emprestado',
         HttpStatus.BAD_REQUEST,
       );
     }
-    return this.loanService.create(createLoanDto);
+
+
+    // valida datas de previsão de devolução e de empréstimo
+    let must_return_date = moment(createLoanDto.must_return_date);
+    let loan_date = moment(createLoanDto.loan_date);
+
+    if (null != createLoanDto.must_return_date) {
+      let is_must_return_date_valid = must_return_date.isValid();
+      if (!is_must_return_date_valid) {
+        throw new HttpException(
+          'Informe uma data de previsão de devolução válida',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+    }
+
+    if (null != createLoanDto.loan_date) {
+      let is_loan_date_valid = loan_date.isValid();
+      if (!is_loan_date_valid) {
+        throw new HttpException(
+          'Informe uma data de empréstimo válida',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+    }
+
+    if (must_return_date.isBefore(loan_date)) {
+      throw new HttpException(
+        'Data de previsão de devolução está anterior a data do empréstimo',
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    // valida data de retorno
+    if (null != createLoanDto.return_date) {
+      let return_date = moment(createLoanDto.return_date);
+      let is_return_date_valid = return_date.isValid();
+      if (!is_return_date_valid) {
+        throw new HttpException(
+          'Informe uma data de devolução válida',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+
+      if (return_date.isBefore(loan_date)) {
+        throw new HttpException(
+          'Data de devolução está anterior a data do empréstimo',
+          HttpStatus.BAD_REQUEST
+        );
+      }
+    }
+
+    const newLoan = await this.loanService.create(createLoanDto);
+
+    return {
+      id: newLoan.id,
+      description: newLoan.description,
+      loan_date: newLoan.loan_date,
+      must_return_date: newLoan.must_return_date,
+      return_date: newLoan.return_date,
+      book: newLoan.book,
+      person: newLoan.person
+    };
   }
 
   @UseGuards(AuthGuard)
@@ -79,7 +139,7 @@ export class LoanController {
     if (person !== undefined) findLoan.person = parseInt(person);
 
     if (description !== undefined) findLoan.description = description;
-    
+
     if (returned !== undefined)
       if (returned == '1')
         findLoan.returned = true;
@@ -115,7 +175,7 @@ export class LoanController {
   async update(@Param('id') id: string, @Body() updateLoanDto: UpdateLoanDto) {
     let bookUpdated = await this.loanService.update(+id, updateLoanDto);
 
-    if(1 == bookUpdated.affected)
+    if (1 == bookUpdated.affected)
       return this.loanService.findOne(+id);
     else
       throw new HttpException(
@@ -135,7 +195,7 @@ export class LoanController {
   async return(@Param('id') id: string, @Body() returnBookDto: ReturnBookDto) {
     let bookUpdated = await this.loanService.returnBook(+id, returnBookDto);
 
-    if(1 == bookUpdated.affected)
+    if (1 == bookUpdated.affected)
       return this.loanService.findOne(+id);
     else
       throw new HttpException(
