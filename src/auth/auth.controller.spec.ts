016864b7e1b1dbd9649ca27eb8a894127c3268b0
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-07-13 14:30:11 -0300

    Add testes para auth

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { mockJwtService } from '../../test/mocks/jwt.service.mock';
+import { mockAuthService } from '../../test/mocks/auth.service.mock';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +12,10 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: AuthService, useValue: mockAuthService }
+      ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -15,4 +24,39 @@ describe('AuthController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('Should call signIn and return a token', async () => {
+    // Cria o dto do usuário para o controller e o retorno do token
+    const dto = { email: 'bruno.f.bevilaqua@gmail.com', password: '123456' };
+    const token = { access_token: 'jwt-token' };
+
+    // Mocka o sign-in do auth service para ele retornar o token criado
+    mockAuthService.signIn.mockResolvedValue(token);
+
+    const result = await controller.signIn(dto);
+
+    // Valida como o mock foi chamado e o resultado
+    expect(mockAuthService.signIn).toHaveBeenCalledWith(dto.email, dto.password);
+    expect(result).toEqual(token);
+  });
+
+  it('Should call signIn and return UnauthorizedException', async () => {
+    // Cria o dto do usuário para o controller, com a senha errada
+    const dto = { email: 'bruno.f.bevilaqua@gmail.com', password: '1234' };
+
+    // Faz com que o signIn retorna uma unauthorized exception
+    mockAuthService.signIn.mockRejectedValue(new UnauthorizedException());
+
+    // Valida o retorno unauthorized
+    await expect(controller.signIn(dto)).rejects.toThrow(UnauthorizedException);
+  });
+
+  it('Should return { isValid: true } from authService', async () => {
+    // Chama o controller
+    const result = await controller.isValid();
+    
+    // Espera o resultado isValid
+    expect(result).toEqual({ isValid: true });
+  });
+
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { mockUserService } from '../../test/mocks/user.service.mock';
+import { mockJwtService } from '../../test/mocks/jwt.service.mock';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: mockUserService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -15,4 +25,41 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Should validate user with correct e-mail and password', async () => {
+    // Cria um mock de retorno que o user service faria
+    const mockUser = {
+      id: 1,
+      email: 'bruno.f.bevilaqua@gmail.com',
+      password: await bcrypt.hash('123', 10),
+    };
+
+    /**
+     * Mocka esse retorno
+     * Quando o service.signin abaixo foi chamado, o findByEmail dentro irá retornar o mock
+     */
+    mockUserService.findByEmail.mockResolvedValue(mockUser);
+    const result = await service.signIn('bruno.f.bevilaqua@gmail.com', '123');
+
+    // valida os retornos
+    expect(result).toHaveProperty('access_token');
+    expect(mockUserService.findByEmail).toHaveBeenCalledWith('bruno.f.bevilaqua@gmail.com');
+  });
+
+  it('Should validate user with wrong e-mail and password', async () => {
+    // Cria o mock de retorno do user service
+    const mockUser = {
+      id: 1,
+      email: 'bruno.f.bevilaqua@gmail.com',
+      password: await bcrypt.hash('123', 10),
+    };
+
+    mockUserService.findByEmail.mockResolvedValue(mockUser);
+
+    // Chama o signin com a senha errada
+    await expect(
+      service.signIn('bruno.f.bevilaqua@gmail.com', 'senhaErrada')
+    ).rejects.toThrow(UnauthorizedException);
+  });
+
 });
