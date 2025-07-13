3f591b77eae4894665af13fcbf46ebb4a6b5be67
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-07-13 13:03:38 -0300

    go back to original test form

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,31 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
-import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: UserService,
-          useValue: {
-            // mock dos métodos utilizados no AuthService
-            findByEmail: jest.fn(),
-            create: jest.fn(),
-          },
-        },
-        {
-          provide: JwtService,
-          useValue: {
-            sign: jest.fn(),
-            verify: jest.fn(),
-          },
-        },
-      ],
+      providers: [AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
