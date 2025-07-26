7d9ed522faded1341dc8355c5564fc656501605c
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-07-25 22:25:52 -0300

    ajustado testes do livro

--- src/book/book.controller.spec.ts
+++ src/book/book.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { mockBookService } from './mocks/book.service.mock';
+import { JwtService } from '@nestjs/jwt';
+import { mockJwtService } from '../auth/mocks/jwt.service.mock';
 
 describe('BookController', () => {
   let controller: BookController;
@@ -8,7 +11,10 @@ describe('BookController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookController],
-      providers: [BookService],
+      providers: [
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: BookService, useValue: mockBookService }
+      ],
     }).compile();
 
     controller = module.get<BookController>(BookController);
--- src/book/book.service.spec.ts
+++ src/book/book.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { mockBookService } from './mocks/book.service.mock';
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      providers: [
+        { provide: BookService, useValue: mockBookService }
+      ],
     }).compile();
 
     service = module.get<BookService>(BookService);
