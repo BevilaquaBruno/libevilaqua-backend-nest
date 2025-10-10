c54607e750fb959a30c73492d3b5757eae6527ad
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-10-09 21:16:02 -0300

    fix person tests

--- src/person/person.controller.spec.ts
+++ src/person/person.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonController } from './person.controller';
 import { PersonService } from './person.service';
+import { mockPersonService } from './mocks/person.service.mock';
+import { JwtService } from '@nestjs/jwt';
+import { mockJwtService } from '../auth/mocks/jwt.service.mock';
 
 describe('PersonController', () => {
   let controller: PersonController;
@@ -8,7 +11,10 @@ describe('PersonController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PersonController],
-      providers: [PersonService],
+      providers: [
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: PersonService, useValue: mockPersonService }
+      ],
     }).compile();
 
     controller = module.get<PersonController>(PersonController);
--- src/person/person.service.spec.ts
+++ src/person/person.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { States } from '../helpers/enum/States.enum';
+import { UpdatePersonDto } from './dto/update-person.dto';
+import { mockPersonService } from './mocks/person.service.mock';
 
 describe('PersonService', () => {
   let service: PersonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PersonService],
+      providers: [{ provide: PersonService, useValue: mockPersonService }],
     }).compile();
 
     service = module.get<PersonService>(PersonService);
@@ -15,4 +19,5 @@ describe('PersonService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
