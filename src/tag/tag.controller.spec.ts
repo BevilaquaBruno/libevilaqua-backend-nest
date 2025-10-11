270be1cf952d62e4ccf76cb879ce4f7bb0adecf4
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-10-11 13:02:15 -0300

    add tag tests

--- src/tag/tag.controller.spec.ts
+++ src/tag/tag.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
+import { JwtService } from '@nestjs/jwt';
+import { mockJwtService } from '../auth/mocks/jwt.service.mock';
+import { mockTagService } from './mocks/tag.service.mock';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { FindTagDto } from './dto/find-tag.dto';
 
 describe('TagController', () => {
   let controller: TagController;
@@ -8,7 +14,10 @@ describe('TagController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TagController],
-      providers: [TagService],
+      providers: [
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: TagService, useValue: mockTagService }
+      ],
     }).compile();
 
     controller = module.get<TagController>(TagController);
@@ -16,5 +25,127 @@ describe('TagController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should create a new tag', async () => {
+    // Cria o dto
+    const publisher: CreateTagDto = {
+      description: 'Tag cadastrada'
+    };
+
+    // Mocka o retorno
+    mockTagService.create.mockResolvedValue({
+      id: 1,
+      ...publisher
+    });
+    const result = await controller.create(publisher);
+
+    expect(result).toEqual({
+      id: 1,
+      ...publisher
+    });
+    expect(mockTagService.create).toHaveBeenCalledWith(publisher);
+  });
+
+  it('should return all tags', async () => {
+    // Cria os mocks e coloca no retorno
+    const list: UpdateTagDto[] = [
+      {
+        id: 1,
+        description: 'Tag cadastrada'
+      },
+      {
+        id: 2,
+        description: 'Tag nova cadastrada'
+      },
+    ];
+    const quantity = list.length;
+
+    // insere os mocks
+    mockTagService.findAll.mockResolvedValue(list);
+    mockTagService.count.mockResolvedValue(quantity);
+
+    // Cria a paginação e requisita
+    const findDto: FindTagDto = {
+      limit: 2,
+      page: 1
+    };
+
+    const result = await controller.findAll(
+      findDto.page.toString(),
+      findDto.limit.toString(),
+    );
+
+    // Valida os retornos
+    expect(result).toEqual({
+      data: list,
+      count: quantity
+    });
+    findDto.page--;
+    expect(mockTagService.findAll).toHaveBeenCalledWith(findDto);
+  });
+
+  it('Should return one tag', async () => {
+    // Cria um mock
+    const publisher: UpdateTagDto = {
+      id: 1,
+      description: 'Tag cadastrada'
+    };
+
+    // Insere os mocks nos serviços
+    mockTagService.findOne.mockResolvedValue(publisher);
+
+    // Cria o mock da consulta e consulta
+    const id = 1;
+    const result = await controller.findOne(id.toString());
+
+    // Valida os dados
+    expect(result).toEqual(publisher);
+    expect(mockTagService.findOne).toHaveBeenCalledWith(id);
+  });
+
+  it('Should edit a tag', async () => {
+    // Cria o dto
+    const id = 1;
+    const dto: UpdateTagDto = {
+      id: 1,
+      description: 'Tag cadastrada'
+    };
+
+    // Mocka o retorno no service e pega o resultado do controller
+    mockTagService.update.mockResolvedValue({
+      raw: [],
+      affected: 1
+    });
+    mockTagService.findOne.mockResolvedValue(dto);
+
+    const result = await controller.update(id.toString(), dto);
+
+    expect(result).toEqual(dto);
+    expect(mockTagService.update).toHaveBeenCalledWith(id, dto);
+  });
+
+  it('Should remove a tag', async () => {
+    // Cria o id
+    const id = 1;
+
+    // Mocka o resultado
+    mockTagService.remove.mockResolvedValue({
+      raw: [],
+      affected: 1
+    });
+    mockTagService.findOne.mockResolvedValue({
+      id: 1,
+      description: 'Tag cadastrada'
+    });
+
+    const result = await controller.remove(id.toString());
+
+    // Valida os retornos
+    expect(result).toEqual({
+      statusCode: 200,
+      message: 'Tag deletada com sucesso.',
+    });
+    expect(mockTagService.remove).toHaveBeenCalledWith(id);
   });
 });
--- src/tag/tag.service.spec.ts
+++ src/tag/tag.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TagService } from './tag.service';
+import { mockTagService } from './mocks/tag.service.mock';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { FindTagDto } from './dto/find-tag.dto';
 
 describe('TagService', () => {
   let service: TagService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TagService],
+      providers: [{ provide: TagService, useValue: mockTagService }],
     }).compile();
 
     service = module.get<TagService>(TagService);
@@ -14,5 +18,122 @@ describe('TagService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Should create a tag', async () => {
+    // Cria o mock
+    const mock: CreateTagDto = {
+      description: 'Tag description'
+    };
+
+    const mockResolved: UpdateTagDto = {
+      id: 1,
+      ...mock
+    }
+
+    // Coloca no resolve
+    mockTagService.create.mockResolvedValue(mockResolved);
+
+    // Chama e valida
+    const result = await service.create({
+      description: 'Tag description'
+    });
+
+    expect(result).toEqual(mockResolved)
+    expect(mockTagService.create).toHaveBeenCalledWith({
+      description: 'Tag description'
+    });
+  });
+
+  it('Should return a list with all tags', async () => {
+    // Cria o mock da lista e coloca no resolve
+    const mockList = [
+      {
+        id: 1,
+        description: 'Tag description'
+      },
+      {
+        id: 2,
+        description: 'Tag description 2'
+      }
+    ];
+    mockTagService.findAll.mockResolvedValue(mockList);
+
+    // Cria a paginação e recebe o retorno;
+    const findDto: FindTagDto = {
+      limit: 2,
+      page: 1
+    };
+    const result = await service.findAll(findDto);
+
+    // Valida os retornos
+    expect(result).toEqual(mockList);
+    expect(mockTagService.findAll).toHaveBeenCalledWith({ limit: 2, page: 1 });
+  });
+
+  it('Should return a tag', async () => {
+    // Cria o mock
+    const mock = {
+      id: 1,
+      description: 'Tag description'
+    };
+    mockTagService.findOne.mockResolvedValue(mock);
+
+    // Consulta
+    const result = await service.findOne(1);
+
+    // Valida os retornos
+    expect(result).toEqual(mock);
+    expect(mockTagService.findOne).toHaveBeenCalledWith(1);
+  });
+
+  it('Should update a tag', async () => {
+    // Cria o mock
+    const mock: UpdateTagDto = {
+      id: 1,
+      description: 'Tag description'
+    };
+    mockTagService.update.mockResolvedValue(mock);
+
+    // Chama a edição do gênero
+    const result = await service.update(1, {
+      id: 1,
+      description: 'Tag description'
+    });
+
+    // Valida o retorno
+    expect(result).toEqual(mock);
+    expect(mockTagService.update).toHaveBeenCalledWith(1, {
+      id: 1,
+      description: 'Tag description'
+    });
+  });
+
+  it('Should remove a tag', async () => {
+    // Cria o mock de retorno e coloca no delete
+    const mockDelete = {
+      raw: [],
+      affected: 1
+    };
+    mockTagService.remove.mockResolvedValue(mockDelete);
+
+    // Deleta
+    const result = await service.remove(1);
+
+    // Valida os retornos
+    expect(result).toEqual({ raw: [], affected: 1 });
+    expect(mockTagService.remove).toHaveBeenCalledWith(1);
+  });
+
+  it('Should return a count of tags', async () => {
+    // Coloca um mock no count
+    mockTagService.count.mockResolvedValue(1);
+
+    // Chama o count
+    const result = await service.count();
+
+    // Valida os retornos
+    expect(result).toEqual(1);
+    expect(mockTagService.count).toHaveBeenCalledWith();
   });
 });
