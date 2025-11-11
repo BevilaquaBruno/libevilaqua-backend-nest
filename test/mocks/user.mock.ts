a17eccc011ce06fc01186691bc0be811113408e0
Bruno Bevilaqua <bbbevilaqua@gmail.com>
2025-11-10 22:42:16 -0300

    (new) Testes para o usuário

---
+++ test/mocks/user.mock.ts
@@ -0,0 +1,17 @@
+export var user1 = {
+  id: 2,
+  name: "Bruno Temp",
+  email: "biblioteca-temp@tuamaeaquelaursa.com",
+  password: "123",
+  verify_password: "123",
+  language: "pt-br"
+}
+
+export var user2 = {
+  id: 3,
+  name: "Bruno Temp",
+  email: "biblioteca-temp2@tuamaeaquelaursa.com",
+  password: "123",
+  verify_password: "123",
+  language: "pt-br"
+}
